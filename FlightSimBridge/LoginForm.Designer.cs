e1db12c0effcab0c64e7551cf4c8b07a35f71293
hammad-nasir <hammadnasir@hotmail.co.uk>
2023-07-30 21:44:57 -0500

    Login Form added

---
+++ FlightSimBridge/LoginForm.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace FlightSimBridge
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginHeading = new System.Windows.Forms.Label();
+            this.emailPanel = new System.Windows.Forms.Panel();
+            this.passwordPanel = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.rememberMeChk = new System.Windows.Forms.CheckBox();
+            this.emailTxt = new System.Windows.Forms.TextBox();
+            this.passwordTxt = new System.Windows.Forms.TextBox();
+            this.registerBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loginHeading
+            // 
+            this.loginHeading.AutoSize = true;
+            this.loginHeading.Font = new System.Drawing.Font("Montserrat Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginHeading.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.loginHeading.Location = new System.Drawing.Point(122, 137);
+            this.loginHeading.Name = "loginHeading";
+            this.loginHeading.Size = new System.Drawing.Size(67, 22);
+            this.loginHeading.TabIndex = 1;
+            this.loginHeading.Text = "Log In";
+            // 
+            // emailPanel
+            // 
+            this.emailPanel.BackColor = System.Drawing.Color.Black;
+            this.emailPanel.Location = new System.Drawing.Point(26, 248);
+            this.emailPanel.Name = "emailPanel";
+            this.emailPanel.Size = new System.Drawing.Size(236, 1);
+            this.emailPanel.TabIndex = 3;
+            // 
+            // passwordPanel
+            // 
+            this.passwordPanel.BackColor = System.Drawing.Color.Black;
+            this.passwordPanel.Location = new System.Drawing.Point(26, 321);
+            this.passwordPanel.Name = "passwordPanel";
+            this.passwordPanel.Size = new System.Drawing.Size(236, 1);
+            this.passwordPanel.TabIndex = 3;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::FlightSimBridge.Properties.Resources.padlock;
+            this.pictureBox3.Location = new System.Drawing.Point(26, 292);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::FlightSimBridge.Properties.Resources.user;
+            this.pictureBox2.Location = new System.Drawing.Point(26, 219);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FlightSimBridge.Properties.Resources.Logo_modified;
+            this.pictureBox1.Location = new System.Drawing.Point(102, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(87, 71);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.loginBtn.FlatAppearance.BorderSize = 0;
+            this.loginBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.loginBtn.ForeColor = System.Drawing.Color.White;
+            this.loginBtn.Location = new System.Drawing.Point(26, 377);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(236, 33);
+            this.loginBtn.TabIndex = 4;
+            this.loginBtn.Text = "Log In";
+            this.loginBtn.UseVisualStyleBackColor = false;
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
+            // 
+            // rememberMeChk
+            // 
+            this.rememberMeChk.AutoSize = true;
+            this.rememberMeChk.Location = new System.Drawing.Point(26, 339);
+            this.rememberMeChk.Name = "rememberMeChk";
+            this.rememberMeChk.Size = new System.Drawing.Size(94, 17);
+            this.rememberMeChk.TabIndex = 5;
+            this.rememberMeChk.Text = "Remember me";
+            this.rememberMeChk.UseVisualStyleBackColor = true;
+            // 
+            // emailTxt
+            // 
+            this.emailTxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.emailTxt.ForeColor = System.Drawing.Color.Black;
+            this.emailTxt.Location = new System.Drawing.Point(58, 231);
+            this.emailTxt.Name = "emailTxt";
+            this.emailTxt.Size = new System.Drawing.Size(204, 13);
+            this.emailTxt.TabIndex = 6;
+            // 
+            // passwordTxt
+            // 
+            this.passwordTxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.passwordTxt.ForeColor = System.Drawing.Color.Black;
+            this.passwordTxt.Location = new System.Drawing.Point(57, 304);
+            this.passwordTxt.Name = "passwordTxt";
+            this.passwordTxt.Size = new System.Drawing.Size(204, 13);
+            this.passwordTxt.TabIndex = 7;
+            this.passwordTxt.UseSystemPasswordChar = true;
+            // 
+            // registerBtn
+            // 
+            this.registerBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.registerBtn.FlatAppearance.BorderSize = 0;
+            this.registerBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.registerBtn.ForeColor = System.Drawing.Color.White;
+            this.registerBtn.Location = new System.Drawing.Point(26, 425);
+            this.registerBtn.Name = "registerBtn";
+            this.registerBtn.Size = new System.Drawing.Size(236, 33);
+            this.registerBtn.TabIndex = 8;
+            this.registerBtn.Text = "Register";
+            this.registerBtn.UseVisualStyleBackColor = false;
+            this.registerBtn.Click += new System.EventHandler(this.registerBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.BackColor = System.Drawing.Color.White;
+            this.exitBtn.FlatAppearance.BorderSize = 0;
+            this.exitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitBtn.ForeColor = System.Drawing.Color.Black;
+            this.exitBtn.Location = new System.Drawing.Point(209, 477);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(71, 33);
+            this.exitBtn.TabIndex = 9;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = false;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(292, 560);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.registerBtn);
+            this.Controls.Add(this.passwordTxt);
+            this.Controls.Add(this.emailTxt);
+            this.Controls.Add(this.rememberMeChk);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.passwordPanel);
+            this.Controls.Add(this.emailPanel);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.loginHeading);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label loginHeading;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel emailPanel;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Panel passwordPanel;
+        private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.CheckBox rememberMeChk;
+        private System.Windows.Forms.TextBox emailTxt;
+        private System.Windows.Forms.TextBox passwordTxt;
+        private System.Windows.Forms.Button registerBtn;
+        private System.Windows.Forms.Button exitBtn;
+    }
+}
--- FlightSimBridge/LoginForm.cs
+++ FlightSimBridge/LoginForm.cs
@@ -1,6 +1,9 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -9,30 +12,35 @@ using System.Windows.Forms;
 
 namespace FlightSimBridge
 {
-    internal static class Program
+    public partial class LoginForm : Form
     {
         private static string FlightSimWebAppUrl = "http://localhost:5233/";
-
-        [STAThread]
-        static async Task Main()
+        public LoginForm()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            // Authenticate the user and obtain the JWT token
-            //var token = await AuthenticateUser("a@test.com", "123456");
-            //if (token != null)
-            //{
-            //    // Start the FlightSimBridge and pass the token to Form1
-            //    Application.Run(new Form1(token));
-            //    //MessageBox.Show(token);
-            //}
-            //else
-            //{
-            //    MessageBox.Show("Authentication failed.");
-            //}
+            InitializeComponent();
 
-            LoginForm loginForm = new LoginForm();
-            Application.Run(loginForm);
+        }
+
+        private async void loginBtn_Click(object sender, EventArgs e)
+        {
+            string email = emailTxt.Text;
+            string password = passwordTxt.Text;
+            bool rememberMe = rememberMeChk.Checked;
+
+            var token = await AuthenticateUser(email, password);
+
+            if (token != null)
+            {
+                this.Hide();
+                Form1 form1 = new Form1(token);
+                form1.Closed += (s, args) => this.Close();
+                form1.Show();
+            }
+            else
+            {
+                MessageBox.Show("Invalid email or password. Please try again.", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
         }
 
         private static async Task<string> AuthenticateUser(string email, string password)
@@ -74,7 +82,27 @@ namespace FlightSimBridge
             public string data { get; set; }
             public bool success { get; set; }
             public string message { get; set; }
-  
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://localhost:5233/register");
+        }
+
+        private void registerBtn_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://localhost:5233/register");
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
